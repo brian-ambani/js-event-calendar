74558b6d91aef6fc82f7953af4dcb780c6c47a76
brian-ambani <brainambani1@gmail.com>
2023-09-20 14:53:39 +0300

    updated all files

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+// selectors
+
+
+// EventListeners
+
+
+
+// functions
+
 const drawBlankCalendar = () => {
-    
+    for (let i = 0; i<35; i++){
+        const day = document.createElement('div');
+        day.classList.add('day');
+
+        const dayText = document.createElement('p');
+        dayText.classList.add('day-text');
+
+        const dayNumber = Document.createElement('p');
+        dayNumber.classList.add('day-number');
+
+        const eventName = document.createElement('small');
+        eventName.classList.add('event-name');
+    }   
 }
